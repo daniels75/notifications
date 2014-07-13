a58264aab64882333041839e830f429b6db27730
daniel.sadowski <daniel.sadowski75@gmail.com>
2014-07-13 15:50:59 +0200

    Moved a xml configuration to AppConfig

--- src/main/java/org/daniels/examples/notification/config/AppConfig.java
+++ src/main/java/org/daniels/examples/notification/config/AppConfig.java
@@ -6,11 +6,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.stereotype.Controller;
 
 @Configuration
 @ComponentScan(basePackages = { "org.daniels.examples.notification" }, 
 	excludeFilters = { @ComponentScan.Filter(value = Controller.class, type = FilterType.ANNOTATION) })
+@ImportResource("classpath*:/applicationContext.xml")
 @EnableAspectJAutoProxy
 public class AppConfig {
 	
